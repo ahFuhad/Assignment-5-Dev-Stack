c62729469b01d45a898e25bfc931c3cbdc75914d
ahFuhad <amirhamzafuhad1234@gmail.com>
2026-09-13 23:58:09 +0600

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import TechnologySection from "./components/TechnologySection"
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <TechnologySection />
       <Footer />
     </>
   );
